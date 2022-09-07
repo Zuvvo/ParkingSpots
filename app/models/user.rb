9657d66f9ecbfe44b8e0fe03a97491c2594116a1
Zuvvo <Zuvvo4@gmail.com>
2022-09-08 01:54:23 +0200

    registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must be valid email address"}
+  validates :password, presence: true
+
 end
