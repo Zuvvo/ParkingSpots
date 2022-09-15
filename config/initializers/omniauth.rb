3a1da9c75a259cdc3923d340b6b208e5544c299c
Zuvvo <Zuvvo4@gmail.com>
2022-09-15 16:54:18 +0200

    slack authentication wip

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, Rails.application.credentials.dig(:slack, :api_key), Rails.application.credentials.dig(:slack, :api_secret), scope: 'identity.basic'
+  provider :slack,
+           Rails.application.credentials.dig(:slack, :client_id),
+           Rails.application.credentials.dig(:slack, :client_secret),
+           scope: 'identity.basic',
+           callback_path: '/auth/slack/callback'
 end
