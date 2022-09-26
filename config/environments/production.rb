9d49b681741fb3ed5cdbf2cc55c1fe3f70f62605
Zuvvo <Zuvvo4@gmail.com>
2022-09-26 17:43:09 +0200

    heroku fix test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.credentials.dig(:mailer, :user_name),
+    :password => Rails.application.credentials.dig(:mailer, :password),
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
