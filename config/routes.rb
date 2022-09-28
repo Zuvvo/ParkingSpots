77e82abe0384ef6671c852efd696e94c39420515
Zuvvo <Zuvvo4@gmail.com>
2022-09-28 13:54:14 +0200

    registration confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,6 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  get "sign_up", to: "registration#new"
-  post "sign_up", to: "registration#create"
-
-  get "log_in", to: "sessions#new"
-  post "log_in", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
-
   get "/auth/slack/callback", to: "omniauth_callbacks#slack"
 
   resources :spots
