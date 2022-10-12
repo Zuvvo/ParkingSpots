e366c5fe56e1880aca331dcec1aec8ba9aef1af5
Zuvvo <Zuvvo4@gmail.com>
2022-10-12 13:22:30 +0200

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def set_current_user
-    if session[:user_id]
-      Current.user = User.find_by(uid: session[:user_uid])
-    end
-  end
 
   protected
 
